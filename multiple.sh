54cdce2942c615374b99756ecfbb9280c6c41550
halva-dev <hhalva.dev@gmail.com>
2025-04-08 22:24:08 +0300

    update

--- multiple.sh
+++ multiple.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-RED='\033[0;31M'
-GREEN='\033[0;32M'
-YELLOW='\033[0;33M'
-BLUE='\033[0;34M'
-MAGENTA='\033[0;35M'
-CYAN='\033[0;36M'
-NC='\033[0M' #RESET COLOR
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Нет цвета (сброс цвета)
 
 # Check if curl is installed and install it if not
 if ! command -v curl &> /dev/null; then
