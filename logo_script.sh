89565b01817f9c36ae169b5dbcbeb6a217863a7d
halva-dev <hhalva.dev@gmail.com>
2025-04-08 20:01:00 +0300

    Add files via upload

---
+++ logo_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e '\e[0;32m' #text color
+echo -e '.:.      ::       ::.      .::    .::       .::    ::.'
+echo -e 'P@J     :B#^     !#&#^     !&B.   :G&?     .G&7   !#&#^'
+echo -e 'P@J     :#&~    ^#B7&B:    !@#.    ~&&~    Y@5   ^#B7&B:'
+echo -e 'P@GJJJJJY&&~   .G&~ 7@P.   !@#.     7@G.  7@G.  .B&~ 7@P.'
+echo -e 'P@G?????J&&~  .P@P~~!B@Y   !@#.      Y@Y ^&#^   P@P~!!B@Y'
+echo -e 'P@J     :#&~  J@BYYYYY#@?  !@#.      .G@7G@!   J@BYYYYY#@?'
+echo -e 'P@J     :#&^ 7@B:     ~&&~ !@&PPPPP^  ^#@@?   7@B:     ~&&~'
+echo -e '^~:     .^^. ^~:       ^~: .^~~~~~~.   :~^    ^~:       ^~:'
+echo -e '\e[0m' #reset color
